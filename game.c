95d4f2357b3b7c85aece9f63309fed37ac6d54b1
Martin <martin.jokl123@gmail.com>
2026-08-08 19:19:05 +0200

    Add game struct

---
+++ game.c
@@ -0,0 +1,24 @@
+#include "game.h"
+
+
+Game *createGame(unsigned int width, unsigned int height) {
+
+}
+
+void freeGame(Game *game) {
+
+}
+
+void initGame(Game *game) {
+
+}
+
+void processGameInput(Game *game, float deltaTime) {
+
+}
+void updateGame(Game *game, float deltaTime) {
+
+}
+void renderGame(Game *game) {
+
+}
---
+++ game.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdbool.h>
+
+typedef enum {
+    GAME_ACTIVE,
+    GAME_MENU,
+    GAME_WIN
+} GameState;
+
+typedef struct {
+    GameState state;
+    bool keys[1024];
+    unsigned int width, height;
+} Game;
+
+Game *createGame(unsigned int width, unsigned int height);
+void freeGame(Game *game);
+
+void initGame(Game *game);
+
+void processGameInput(Game *game, float deltaTime);
+void updateGame(Game *game, float deltaTime);
+void renderGame(Game *game);
