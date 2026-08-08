e8aa234611a2c11b6069b828f22e419538c9ff19
Martin <martin.jokl123@gmail.com>
2026-08-08 22:42:35 +0200

    Add code to show a window

--- main.c
+++ main.c
@@ -1,5 +1,77 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
+#include "game.h"
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+
+const unsigned int screenWidth = 800;
+const unsigned int screenHeight = 600;
+
+Game *game;
 
 int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    glfwWindowHint(GLFW_RESIZABLE, false);
+
+    GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "Breakout", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    glfwSetKeyCallback(window, keyCallback);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    glViewport(0, 0, screenWidth, screenHeight);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    game = createGame(screenWidth, screenHeight);
+    initGame(game);
+
+    float deltaTime = 0.0f;
+    float lastFrame = 0.0f;
+    while (!glfwWindowShouldClose(window))
+    {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        glfwPollEvents();
+
+        processGameInput(game, deltaTime);
+
+        updateGame(game, deltaTime);
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        renderGame(game);
+
+        glfwSwapBuffers(window);
+    }
+
+    freeGame(game);
+    glfwTerminate();
     return 0;
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+    if (key >= 0 && key < 1024)
+    {
+        if (action == GLFW_PRESS)
+            game->keys[key] = true;
+        else if (action == GLFW_RELEASE)
+            game->keys[key] = false;
+    }
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
 }
